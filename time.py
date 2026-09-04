07a24ecc86c5c607d7ee131abe0d93df313c08c0
akshaykhedkar9797-rgb <akshaykhedkar9797@gmail.com>
2026-09-04 11:48:28 +0530

    Create time.py

---
+++ time.py
@@ -0,0 +1,10 @@
+# Initialize the counter
+counter = 10
+
+# Count down from 10 to 1
+while counter >= 1:
+    print(counter)
+    counter -= 1
+
+# Display message after countdown
+print("Blast off!")
